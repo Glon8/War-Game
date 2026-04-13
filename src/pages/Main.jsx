869ae86f96ef4474a8c162d5969e549154e8906f
Daniel Ruz <Dani.Ruzanov@gmail.com>
2026-04-13 12:04:11 +0300

    dublicate removal

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -10,19 +10,11 @@ export default function Main() {
 
     const jokerCard = { power: 15, type: 5, state: false };
 
-    return (<div className='w-per-20 h-per-20 justify-i-c align-c-c bg-green'>
+    return (<div className='w-per-20 h-per-20 justify-i-c align-c-c bg-c-green'>
 
-        <div className=' w-per-20 h-per-20
-            max-w-14
-            max-h-12
-            p-x-5 p-y-9
-            flex flex-c
-            align-i-c
-            r-4
-            bg-t-5'
-        >
+        <div className=' w-per-20 h-per-20 max-w-14 max-h-12 p-x-5 p-y-9 flex flex-c align-i-c r-4 bg-t-5 box-sh-md-o-3' >
 
-            <h1 className='w-rem-5 text-orange-50 underline decoration-black decoration-wavy underline-offset-8 decoration-4 text-[5rem] drop-shadow-md font-bold align-middle text-center'>War-Game</h1>
+            <h1 className='w-per-20 c-red text-a-c text-d-und text-d-s-wavy text-d-c-red text-d-t-px-1 font-s-12 drop-shadow-md font-bold align-middle'>War-Game</h1>
             <div className='mt-[10%] flex justify-between'>
                 {/*
                 <Card card={jokerCard} customDesc={'SCORE'} turn={true} onClick={toScore} />
